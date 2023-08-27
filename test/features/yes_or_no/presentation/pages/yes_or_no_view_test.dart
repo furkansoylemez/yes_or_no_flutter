cb08bb859ae92ab34e400d39670e128e573802f9
Furkan Söylemez <furkansoylemez@outlook.com>
2023-08-27 13:40:56 +0300

    Update yes_or_no_view_test.dart

--- test/features/yes_or_no/presentation/pages/yes_or_no_view_test.dart
+++ test/features/yes_or_no/presentation/pages/yes_or_no_view_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:formz/formz.dart';
-import 'package:mocktail/mocktail.dart'; // You'll need to include the mocktail package in your pubspec.yaml
+import 'package:mocktail/mocktail.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 import 'package:yes_or_no/core/error/failures.dart';
 import 'package:yes_or_no/core/extensions/app_localizations.dart';
@@ -22,8 +22,7 @@ void main() {
 
     setUp(() {
       answerBloc = MockAnswerBloc();
-      when(() => answerBloc.state)
-          .thenReturn(const AnswerState()); // Default state
+      when(() => answerBloc.state).thenReturn(const AnswerState());
     });
 
     tearDown(() {
